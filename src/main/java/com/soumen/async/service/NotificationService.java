04cc1fc1dc4a6c12778f4672ef7c24a757d2da86
s0umen <soumen.raj3@gmail.com>
2022-06-18 21:31:32 +0530

    Exception and sleep method commented out!!!

--- src/main/java/com/soumen/async/service/NotificationService.java
+++ src/main/java/com/soumen/async/service/NotificationService.java
@@ -3,7 +3,6 @@ package com.soumen.async.service;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import javax.websocket.server.PathParam;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -12,11 +11,11 @@ public class NotificationService {
     @Async
     public void sendTextMessage(String mobileNumber){
 
-        sleep(10);
+        //sleep(10);
         // Call 3rd party Api to send a text message
         System.out.println("Text Message Sent via a different thread :: " + Thread.currentThread().getName());
 
-        System.out.println(100/0); // introducing an Arithmetic Exception to see how things behave
+        //System.out.println(100/0); // introducing an Arithmetic Exception to see how things behave
     }
 
     private void sleep(int args){
